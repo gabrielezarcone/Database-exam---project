c8bfc2e1c8dea954b5756ae31d9f606e9460a356
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-01 12:46:27 +0200

    initial commit

---
+++ Database/Database_definition.sql
@@ -0,0 +1,18 @@
+
+CREATE USER administration;
+
+CREATE DATABASE crowdsourcing;
+
+CREATE SCHEMA crowdsourcing;
+
+-- DOMAINS --
+
+CREATE DOMAIN keyword_type AS VARCHAR(50);
+
+-- TABLES --
+
+
+
+
+
+
