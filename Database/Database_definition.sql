d4a02264f56fce66da1205ed6e3fc873a1cc41e2
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-01 13:04:44 +0200

    entity tables defined

--- Database/Database_definition.sql
+++ Database/Database_definition.sql
@@ -11,7 +11,43 @@ CREATE DOMAIN keyword_type AS VARCHAR(50);
 
 -- TABLES --
 
-
+CREATE TABLE keyword(
+    keyword VARCHAR(50) PRIMARY KEY,
+    type keyword_type NOT NULL 
+);
+CREATE TABLE task(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(280),
+    title VARCHAR(50),
+    n_workers INTEGER,
+    threshold INTEGER,
+    valid_bit BOOLEAN,
+    pay   ,
+    campaign INTEGER REFERENCE campaign(id)
+);
+CREATE TABLE answer(
+    value VARCHAR(100) PRIMARY KEY
+);
+CREATE TABLE worker(
+    user_name VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(20)? UNIQUE NOT NULL 
+);
+CREATE TABLE requester(
+    user_name VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(20)? UNIQUE NOT NULL 
+);
+CREATE TABLE campaign(
+    id SERIAL PRIMARY KEY,
+    registration_start_date DATE NOT NULL,
+    registration_end_date DATE NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+------------------------------------------------
+CREATE TABLE requires();
+CREATE TABLE recives();
+CREATE TABLE has();
+CREATE TABLE join();
 
 
 
