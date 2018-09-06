1acd90f103f51c1670126d5066300664b07752e9
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-06 23:35:42 +0200

    updated the Database_definition.sql file to match with the new ER diagram

--- Database/Database_definition.sql
+++ Database/Database_definition.sql
@@ -11,9 +11,19 @@ CREATE DOMAIN keyword_type AS VARCHAR(50);
 
 -- TABLES --
 
+CREATE TABLE campaign(
+    id SERIAL PRIMARY KEY,
+    registration_start_date DATE NOT NULL,
+    registration_end_date DATE NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
 CREATE TABLE keyword(
     keyword VARCHAR(50) PRIMARY KEY,
     type keyword_type NOT NULL 
+);
+CREATE TABLE pay(
+    type VARCHAR(50) PRIMARY KEY
 );
 CREATE TABLE task(
     id SERIAL PRIMARY KEY,
@@ -22,28 +32,35 @@ CREATE TABLE task(
     n_workers INTEGER,
     threshold INTEGER,
     valid_bit BOOLEAN,
-    pay   ,
-    campaign INTEGER REFERENCE campaign(id)
+    campaign INTEGER,
+    pay_type VARCHAR(50),
+    pay_description VARCHAR(280),
+    FOREIGN KEY (pay_type) REFERENCES pay(type),
+    FOREIGN KEY (campaign) REFERENCES campaign(id)
 );
 CREATE TABLE answer(
-    value VARCHAR(100) PRIMARY KEY
+    task INTEGER,
+    value VARCHAR(100),
+    PRIMARY KEY(task,value),
+    FOREIGN KEY(task) REFERENCES task(id)
 );
 CREATE TABLE worker(
     user_name VARCHAR(20) PRIMARY KEY,
-    password VARCHAR(20)? UNIQUE NOT NULL 
+    password VARCHAR(20)? UNIQUE NOT NULL
 );
 CREATE TABLE requester(
     user_name VARCHAR(20) PRIMARY KEY,
     password VARCHAR(20)? UNIQUE NOT NULL 
 );
-CREATE TABLE campaign(
-    id SERIAL PRIMARY KEY,
-    registration_start_date DATE NOT NULL,
-    registration_end_date DATE NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL
-);
 ------------------------------------------------
+CREATE TABLE choose(
+    worker VARCHAR(20),
+    task INTEGER,
+    answer VARCHAR(100),
+    PRIMARY KEY(worker),
+    FOREIGN KEY(worker) REFERENCES worker(user_name),
+    FOREIGN KEY(task,answer) REFERENCES answer(task,value)
+);
 CREATE TABLE requires(
     task INTEGER,
     keyword VARCHAR(50),
@@ -52,7 +69,12 @@ CREATE TABLE requires(
     FOREIGN KEY(keyword) REFERENCES keyword(keyword)
 );
 CREATE TABLE recives(
-    
+    task INTEGER,
+    worker VARCHAR(20),
+    valid_bit_user BOOLEAN,
+    PRIMARY KEY(task,worker),
+    FOREIGN KEY(task) REFERENCES task(id),
+    FOREIGN KEY(worker) REFERENCES worker(user_name),
 );
 CREATE TABLE has(
     worker VARCHAR(20),
@@ -62,14 +84,11 @@ CREATE TABLE has(
     FOREIGN KEY(worker) REFERENCES worker(user_name),
     FOREIGN KEY(keyword) REFERENCES keyword(keyword)
 );
-CREATE TABLE join(
+CREATE TABLE joins(
     worker VARCHAR(20),
     campaign INTEGER,
     PRIMARY KEY(worker,campaign),
     FOREIGN KEY(worker) REFERENCES worker(user_name),
-    FOREIGN KEY(campaign) REFERENCES campaign(id),
+    FOREIGN KEY(campaign) REFERENCES campaign(id)
 );
-
-
-
 
