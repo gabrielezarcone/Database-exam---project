71d71bd83494f21fe1d27fcfe3c82fc780021450
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-08 17:38:11 +0200

    initial commit

---
+++ WebSite/lib/header.php
@@ -0,0 +1,3 @@
+<meta name="keywords" content="crowd, croedsourcing, pay, task, work, request">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/uikit.min.css" rel="stylesheet" type="text/css">
