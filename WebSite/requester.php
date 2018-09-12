6d524a8bd9e6853cbf63950859ae379dbdca606b
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-12 13:46:46 +0200

    requester page structure + worker page refactoring

--- WebSite/requester.php
+++ WebSite/requester.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include_once("lib/header.php")?>
-    <?php include_once("lib/title_worker.php"); ?>
+    <?php include_once("lib/title_requester.php"); ?>
     <?php include_once("lib/navbar.php")?>
     <title>usernameRRRR@@#@#@#@#@#@]@[@#@#</title>
 </head>
 <body>
-    <div uk-grid style="margin-top:2%;">
+    <div uk-grid style="margin-top:2%;" >
         <div class="uk-width-1-4 scrollable-side ">
-            <h2 style="color: var(--worker-color); text-align: center;">Campaigns</h2>
+            <h2 style="color: var(--requester-color); text-align: center;">Campaigns</h2>
             <div class="uk-flex uk-flex-column">
                 <a href="#"><div class="uk-card uk-card-default uk-card-body uk-flex-wrap-stretch">Item 1</div></a>
                 <a href="#"><div class="uk-card uk-card-default uk-card-body uk-margin-top uk-flex-wrap-stretch">Item 2</div></a>
@@ -28,46 +28,47 @@
 
         <div class="uk-width-1-4"></div>
 
-        <div class="uk-width-1-2 card-container">
-            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card">
-                <h1 class="card">Title</h1>
+        <div class="uk-width-1-2 cards-container">
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
+                <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            </div>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-requester uk-margin">
+                <h1 class="card" style="color: white;">Title</h1>
                 <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
             </div>
         </div>
         <div class="uk-width-1-4 card-container">
             <div class="uk-card uk-card-default uk-card-body ">
-                <h2 style="color: var(--worker-color)">Worker info</h2>
+                <h2 style="color: var(--requester-color)">Worker info</h2>
                 <ul class="uk-list">
                     <li><h4>User_Name: </h4>nome@@@@</li>
                     <li><h4>Number of joined campaings: </h4>@@@@</li>
                     <li><h4>Success rate: </h4>@@@@</li>
                 </ul>
             </div>
-        </div>
-    </div>
-    <div uk-grid style="margin-top:2%;">
-        <div class="uk-width-1-4"></div>
-        <div class="uk-width-1-2 card-container">
-            <div uk-grid>
-                <div class="uk-width-1-3"></div>
-                <form class="uk-width-1-3">
-                    <ul class="uk-list">
-                        <li><h2><input class="uk-radio" type="radio" name="radio1" checked> Answer A</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer B</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer C</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer D</h2></li>
-                    </ul>
-                    <button class="uk-button uk-button-default worker">Submit Aswer</button>
-                </form>
-                <div class="uk-width-1-3">
-                    <button class="uk-button uk-button-default">Skip</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="uk-width-1-4" style="padding-right:3%;padding-left:3%;">
-            <div class="uk-card uk-card-default uk-card-body ">
-                <h2 style="color: var(--worker-color)">Skills</h2>
+            <div class="uk-card uk-card-default uk-card-body uk-margin-top">
+                <h2 style="color: var(--requester-color)">Skills</h2>
                 <ul class="uk-list">
                     <li><h4>skill-1: </h4>nome@@@@</li>
                     <li><h4>skill-2: </h4>@@@@</li>
@@ -76,5 +77,6 @@
             </div>
         </div>
     </div>
+    
 </body>
 </html>
--- WebSite/worker.php
+++ WebSite/worker.php
@@ -29,10 +29,20 @@
         <div class="uk-width-1-4"></div>
 
         <div class="uk-width-1-2 card-container">
-            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card">
-                <h1 class="card">Title</h1>
+            <div class="uk-card uk-card-default uk-card-body uk-animation-scale-down uk-width-expand card-worker">
+                <h1 class="card" style="color: #ffffff;">Title</h1>
                 <h2 class="card" style="color: white;">Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
             </div>
+             <form class="uk-margin">
+                <ul class="uk-list">
+                    <li><h2><input class="uk-radio" type="radio" name="radio1" checked> Answeranswer A</h2></li>
+                    <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer B</h2></li>
+                    <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer C</h2></li>
+                    <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer D</h2></li>
+                </ul>
+                <button class="uk-button uk-button-default worker">Submit Aswer</button>
+                <button class="uk-button uk-button-default ">Skip</button>
+            </form>
         </div>
         <div class="uk-width-1-4 card-container">
             <div class="uk-card uk-card-default uk-card-body ">
@@ -43,30 +53,7 @@
                     <li><h4>Success rate: </h4>@@@@</li>
                 </ul>
             </div>
-        </div>
-    </div>
-    <div uk-grid style="margin-top:2%;">
-        <div class="uk-width-1-4"></div>
-        <div class="uk-width-1-2 card-container">
-            <div uk-grid>
-                <div class="uk-width-1-3"></div>
-                <form class="uk-width-1-3">
-                    <ul class="uk-list">
-                        <li><h2><input class="uk-radio" type="radio" name="radio1" checked> Answer A</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer B</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer C</h2></li>
-                        <li><h2><input class="uk-radio" type="radio" name="radio1"> Answer D</h2></li>
-                    </ul>
-                    <button class="uk-button uk-button-default worker">Submit Aswer</button>
-                </form>
-                <div class="uk-width-1-3">
-                    <button class="uk-button uk-button-default">Skip</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="uk-width-1-4" style="padding-right:3%;padding-left:3%;">
-            <div class="uk-card uk-card-default uk-card-body ">
+            <div class="uk-card uk-card-default uk-card-body uk-margin-top">
                 <h2 style="color: var(--worker-color)">Skills</h2>
                 <ul class="uk-list">
                     <li><h4>skill-1: </h4>nome@@@@</li>
@@ -76,5 +63,6 @@
             </div>
         </div>
     </div>
+    
 </body>
 </html>
