814ba9d8342f1ace21cbaeb699265c5311b69f18
Gabriele Zarcone <gabriele.zarcone@studenti.unimi.it>
2018-09-16 22:10:39 +0200

    add create_task function to end_task.php

--- WebSite/end_task.php
+++ WebSite/end_task.php
@@ -7,7 +7,20 @@
         if(preg_match ('/answ./' , $k)==1){
             $_SESSION[task][answers][$k] = $value;
         }
-    }               
+    }       
+    $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($_SESSION[task]));        
+    foreach ($iterator as $k => $value) {
+        if($value!=null){
+            create_task($_SESSION[task][title], $_SESSION[task][description], $_SESSION[task][campaign], $_SESSION[task][n_workers], $_SESSION[task][threshold], $_SESSION[task][pay_type], $_SESSION[task][pay_description]);
+
+            print('<div class="uk-alert-success" uk-alert>
+                                                    <a class="uk-alert-close" uk-close></a>
+                                                    <p>Task '.$_SESSION[task][name].' created</p>
+                                                </div>');
+        }
+    }   
+    
+    print_r($_SESSION);
     
 
 ?>
@@ -26,13 +39,18 @@
     <div uk-grid style="margin-top:2%;">
         <div class="uk-width-1-3"></div>
         <div class="uk-width-1-3">
-            <h1 class="uk-heading-primary">Add answers to task <?php $task[title] ?></h1>
+            <h1 class="uk-heading-primary">Finish <?php $task[title] ?></h1>
             <form action="end_task.php" method="POST">
                 
                 <div class="uk-margin" >
                 
                     <div class="uk-margin">
+                    <?php
                         
+                        if(isset($task[title])&&isset($task[description])&&isset($task[campaign])&&isset($task[n_workers])&&isset($task[threshold])&&isset($task[pay_type])&&isset($task[pay_description])){
+                            
+                        }
+                    ?>
                     </div>
                     
             </form>
